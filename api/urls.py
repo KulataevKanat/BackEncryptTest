a3ab30a65b420caf4fa828f6085f1ab8e1b7b9e8
KulataevKanat <RaketaEnot4466@gmail.com>
2021-06-29 12:18:27 +0600

    added Course endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-# import views
+from api import views
 
 urlpatterns = [
-    # path('/', views..as_view()),
+    path('course/create_course/', views.CreateCourseView.as_view()),
+    path('course/delete_course_by_id/<int:pk>/', views.DeleteCourseByIdView.as_view()),
+    path('course/delete_all_courses/', views.DeleteAllCoursesView.as_view()),
+    path('course/update_course_by_id/<int:pk>/', views.UpdateCourseByIdView.as_view()),
+    path('course/find_all_courses/', views.FindAllCoursesView.as_view()),
+    path('course/find_course_by_id/<int:pk>/', views.FindCourseByIdView.as_view()),
 ]
