60f1a6379773c083461a4842b1f2b1503aefaeb7
KulataevKanat <RaketaEnot4466@gmail.com>
2021-07-12 12:27:57 +0600

    db updated

--- BackEncryptTest/settings.py
+++ BackEncryptTest/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'BackEncrypt',
         'USER': 'postgres',
-        'PASSWORD': '4466567693441',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
